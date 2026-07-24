9297ee69a4645a15408e1c3d1cede2b5f9819a49
Pawcific <98gjkrvv9w@privaterelay.appleid.com>
2026-07-24 15:45:32 +0000

    Update mzt_vm.rs

--- src/mzt_vm.rs
+++ src/mzt_vm.rs
@@ -3,5 +3,5 @@ mod cat_instructions;
 fn main() {
     let instruction = cat_instructions::Instruction::Load(0, 1, 5);
     println!("{}", instruction);
-    #cat_instructions::execute_instruction(instruction);
+    // cat_instructions::execute_instruction(instruction);
 }
